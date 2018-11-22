f1892025ed858048ff1a9a908dbfa18e57835844
jeromechavez <jayrome12@yahoo.com>
2018-11-21 23:04:19 -0800

    Added an event handler that would update state with new tasks #1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import Input from './Input.js'
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
--- src/Input.js
+++ src/Input.js
@@ -2,11 +2,39 @@ import React from 'react'
 import { Component } from 'react'
 
 export default class Input extends Component {
+  constructor(props) {
+    super(props)
+    this.handleSubmit = this.handleSubmit.bind(this)
+    this.state = {
+      tasks: []
+    }
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault()
+    const target = event.target
+    const data = new FormData(target)
+    const newTask = {}
+    for (let pair of data.entries()) {
+      newTask[pair[0]] = pair[1]
+    }
+    const updatedTasks = [...this.state.tasks, newTask]
+    this.setState({tasks: updatedTasks})
+  }
+  
   render() {
     return (
       <div>
-        <input></input>
-        <button>Save</button>
+        <h2>Super Lists</h2>
+        <form onSubmit={this.handleSubmit}>
+          <label>Task</label>
+          <input
+            type="text"
+            name="task"
+          />
+          
+          <button type="submit">Save</button>
+        </form>
       </div>
     )
   }
